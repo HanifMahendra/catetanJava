e5e6934b82acacec802bd11ac582f59f239ce78e
HanifMahendra <hanifwdyt4@gmail.com>
2025-11-02 00:59:53 +0700

    Update runtime polymorphism, getter and setter.

---
+++ Resources/Getter_and_Setter/Car.java
@@ -0,0 +1,56 @@
+package Getter_and_Setter;
+
+public class Car {
+    
+    private String model;
+    private String color;
+    private int price;
+
+    // PERBEDAAN CONSTRUCTOR DAN SETTER
+    // CONSTRUCTOR = Menetapkan state awal yang diperlukan.
+    // SETTER = Memungkinkan perubahan state setelah pembuatan.
+
+    // CONSTRUCTOR
+    Car(String model, String color, int price){
+        this.model = model;
+        this.color = color;
+        this.price = price;
+    }
+
+    // GETTERS
+    String getModel(){
+        return this.model;
+    }
+
+    String getColor(){
+        return this.color;
+    }
+
+    int getPrice(){
+        return this.price;
+    }
+
+//  Bisa begini juga:
+//  String getPrice(){
+//      return "$" + this.price;
+//  }
+
+
+    // SETTERS
+    void setColor(String color){
+        this.color = color;
+    }
+
+    void setPrice(int price){
+        if(price < 0){
+            System.out.println("Price can't be less than zero");
+        }
+        else{
+            this.price = price;
+        }
+    }
+
+    void setModel(String model){
+        this.model = model;
+    }
+}
---
+++ Resources/Getter_and_Setter/Main.java
@@ -0,0 +1,23 @@
+package Getter_and_Setter;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println(); // Biar enak bacanya, kasih space diawal
+
+        // They help protect data and add rules for accessing or modifying them.
+        // GETTERS = Methods that make a field READABLE.
+        // SETTERS = Methods that make a field WRITEABLE.
+
+        Car car =  new Car("Charger", "Yellow", 10000);
+
+        // Tes SETTER:
+        car.setColor("Black");
+        car.setPrice(-5000); // Because the conditional statements equal to false, the price didn't change.
+        car.setPrice(7500); // Because the conditional statement equal to true, the price did change.
+        car.setModel("Corvette");
+        // If you want the 'model' can't be change, change the declaration to 'private final String model;'
+        // also delete the SETTER of Model, and delete the implementation of the SETTER (So you get no errors).
+
+        System.out.println("The price of " + car.getColor() + " " + car.getModel() + " is $" + car.getPrice());
+    }
+}
