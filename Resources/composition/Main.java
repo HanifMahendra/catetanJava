3953a250045e50d3442992857098fc37f2635562
HanifMahendra <hanifwdyt4@gmail.com>
2025-11-14 14:10:10 +0700

    Updated (wrapperclass not yet)

--- Resources/composition/Main.java
+++ Resources/composition/Main.java
@@ -6,7 +6,15 @@ public class Main {
         //               For example, an Engine is "part of" a Car.
         //               Allows complex objects to be constructed from smaller objects.
 
-        
+        Car car = new Car("Corvette",2025,"V8"); // You need this to access the engine.
+
+        System.out.println(car.model);
+        System.out.println(car.year);
+
+        // THIS IS THE COMPOSITION
+        System.out.println(car.engine.type); // To access certain variable from other files in the same package
+
+        car.start();
     }
     
 }
---
+++ Resources/wrapperclass/Main.java
@@ -0,0 +1,9 @@
+package wrapperclass;
+
+public class Main {
+    // Wrapper classes = Allow primitive values (int, char, double, boolean)
+    //                   to be used as objects. "Wrap them in an object"
+    //                   Generally, don't wrap primitives unless you need an object
+    //                   Allows use of Collections Framework and static Utility Methods.
+
+}
